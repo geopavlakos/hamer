9552bce477650bcac3e88b89381be26599c58b84
Georgios Pavlakos <geopavlakos@gmail.com>
2023-12-12 19:50:18 -0800

    Update fetch_training_data.sh

--- fetch_training_data.sh
+++ fetch_training_data.sh
@@ -1,5 +1,5 @@
 # Downloading all tars
-wget -O hamer_training_data_part1.tar.gz https://www.dropbox.com/scl/fi/f249h32hd35x78l058ofy/hamer_training_data_part1.tar.gz?rlkey=puuvwg5ngueaxl4xxwf3yd15a
+wget -O hamer_training_data_part1.tar.gz https://www.dropbox.com/scl/fi/bqq0jheev3626q1wiijs1/hamer_training_data_part1.tar.gz?rlkey=8fv4ktvk7r3txofd90q0trgxr
 wget -O hamer_training_data_part2.tar.gz https://www.dropbox.com/scl/fi/l9l5udalchu0mh4qxnw2t/hamer_training_data_part2.tar.gz?rlkey=i0n2lzix4q6jxmhm4sr5rtmkt
 wget -O hamer_training_data_part3.tar.gz https://www.dropbox.com/scl/fi/6lamcbwt79ri0oj4knwm3/hamer_training_data_part3.tar.gz?rlkey=j5y7ea7xrlu440ud12otaj2ne
 wget -O hamer_training_data_part4a.tar.gz https://www.dropbox.com/scl/fi/vp6cw7he8t0eigjf6001l/hamer_training_data_part4a.tar.gz?rlkey=wylmufft4a5nq3yxep2olifrk
