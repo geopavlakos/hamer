19b3f9704de262754cde4ca8b85ddb5ba3f1adef
ntvthuyen <ntvthuyen@gmail.com>
2024-10-18 22:14:16 +0800

    skip frame

--- video_demo.py
+++ video_demo.py
@@ -81,149 +81,166 @@ def main():
     #pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
     #for img_path in img_paths:
     
+    frame_skip = 5
+    frames = np.array([None]*frame_skip)
     image_size = (1024,1024)
     video=cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'XVID'),30,(512,512))
     frame_num = 0
+    frame_skip_count = 0
     try:
         while cap.isOpened():
-            flag, frame = cap.read()
-            frame_num+=1
-            img_cv2 = frame # cv2.imread(str(img_path))
-
-            img_cv2 = cv2.resize(img_cv2, image_size, 
-                   interpolation = cv2.INTER_LINEAR)
-            # Detect humans in image\
-
-            det_out = detector(img_cv2)
-            img = img_cv2.copy()[:, :, ::-1]
-
-            det_instances = det_out['instances']
-            valid_idx = (det_instances.pred_classes==0) & (det_instances.scores > 0.5)
-            pred_bboxes=det_instances.pred_boxes.tensor[valid_idx].cpu().numpy()
-            pred_scores=det_instances.scores[valid_idx].cpu().numpy()
-
-            # Detect human keypoints for each person
-            vitposes_out = cpm.predict_pose(
-                img,
-                [np.concatenate([pred_bboxes, pred_scores[:, None]], axis=1)],
-            )
-
-            bboxes = []
-            is_right = []
-
-            # Use hands based on hand keypoint detections
-            for vitposes in vitposes_out:
-                left_hand_keyp = vitposes['keypoints'][-42:-21]
-                right_hand_keyp = vitposes['keypoints'][-21:]
-
-                # Rejecting not confident detections
-                keyp = left_hand_keyp
-                valid = keyp[:,2] > 0.5
-                if sum(valid) > 3:
-                    bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
-                    bboxes.append(bbox)
-                    is_right.append(0)
-                keyp = right_hand_keyp
-                valid = keyp[:,2] > 0.5
-                if sum(valid) > 3:
-                    bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
-                    bboxes.append(bbox)
-                    is_right.append(1)
-
-            if len(bboxes) == 0:
-                continue
-
-            boxes = np.stack(bboxes)
-            right = np.stack(is_right)
-
-            # Run reconstruction on all detected hands
-            dataset = ViTDetDataset(model_cfg, img_cv2, boxes, right, rescale_factor=args.rescale_factor)
-            dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
-
-            all_verts = []
-            all_cam_t = []
-            all_right = []
+            frame_num += 1
             
-            for batch in dataloader:
-                batch = recursive_to(batch, device)
-                with torch.no_grad():
-                    out = model(batch)
+            if frame_skip_count <= 4:
+                flag, frame = cap.read()
+                img_cv2 = frame # cv2.imread(str(img_path))
 
-                multiplier = (2*batch['right']-1)
-                pred_cam = out['pred_cam']
-                pred_cam[:,1] = multiplier*pred_cam[:,1]
-                box_center = batch["box_center"].float()
-                box_size = batch["box_size"].float()
-                img_size = batch["img_size"].float()
-                multiplier = (2*batch['right']-1)
-                scaled_focal_length = model_cfg.EXTRA.FOCAL_LENGTH / model_cfg.MODEL.IMAGE_SIZE * img_size.max()
-                pred_cam_t_full = cam_crop_to_full(pred_cam, box_center, box_size, img_size, scaled_focal_length).detach().cpu().numpy()
+                img_cv2 = cv2.resize(img_cv2, image_size, 
+                           interpolation = cv2.INTER_LINEAR)
+                 
+                frames[frame_skip_count] = img_cv2
+                frame_skip_count += 1
+            else:
+                idx = get_less_blurry_image(frames)
+                           
+                #flag, frame = cap.read()
+            
+                img_cv2 = frames[idx] # cv2.imread(str(img_path))
 
-                # Render the result
-                batch_size = batch['img'].shape[0]
-                for n in range(batch_size):
-                    # Get filename from path img_path
-                    #img_fn, _ = os.path.splitext(os.path.basename(img_path))
-                    person_id = int(batch['personid'][n])
-                    white_img = (torch.ones_like(batch['img'][n]).cpu() - DEFAULT_MEAN[:,None,None]/255) / (DEFAULT_STD[:,None,None]/255)
-                    input_patch = batch['img'][n].cpu() * (DEFAULT_STD[:,None,None]/255) + (DEFAULT_MEAN[:,None,None]/255)
-                    input_patch = input_patch.permute(1,2,0).numpy()
+                img_cv2 = cv2.resize(img_cv2, image_size,  
+                       interpolation = cv2.INTER_LINEAR)
+                # Detect humans in image\
 
-                    regression_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
-                                            out['pred_cam_t'][n].detach().cpu().numpy(),
-                                            batch['img'][n],
-                                            mesh_base_color=LIGHT_BLUE,
-                                            scene_bg_color=(1, 1, 1),
-                                            )
+                det_out = detector(img_cv2)
+                img = img_cv2.copy()[:, :, ::-1]
 
-                    if args.side_view:
-                        side_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                det_instances = det_out['instances']
+                valid_idx = (det_instances.pred_classes==0) & (det_instances.scores > 0.5)
+                pred_bboxes=det_instances.pred_boxes.tensor[valid_idx].cpu().numpy()
+                pred_scores=det_instances.scores[valid_idx].cpu().numpy()
+
+                # Detect human keypoints for each person
+                vitposes_out = cpm.predict_pose(
+                    img,
+                    [np.concatenate([pred_bboxes, pred_scores[:, None]], axis=1)],
+                )
+
+                bboxes = []
+                is_right = []
+
+                # Use hands based on hand keypoint detections
+                for vitposes in vitposes_out:
+                    left_hand_keyp = vitposes['keypoints'][-42:-21]
+                    right_hand_keyp = vitposes['keypoints'][-21:]
+
+                    # Rejecting not confident detections
+                    keyp = left_hand_keyp
+                    valid = keyp[:,2] > 0.5
+                    if sum(valid) > 3:
+                        bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
+                        bboxes.append(bbox)
+                        is_right.append(0)
+                    keyp = right_hand_keyp
+                    valid = keyp[:,2] > 0.5
+                    if sum(valid) > 3:
+                        bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
+                        bboxes.append(bbox)
+                        is_right.append(1)
+
+                if len(bboxes) == 0:
+                    continue
+
+                boxes = np.stack(bboxes)
+                right = np.stack(is_right)
+
+                # Run reconstruction on all detected hands
+                dataset = ViTDetDataset(model_cfg, img_cv2, boxes, right, rescale_factor=args.rescale_factor)
+                dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
+
+                all_verts = []
+                all_cam_t = []
+                all_right = []
+                
+                for batch in dataloader:
+                    batch = recursive_to(batch, device)
+                    with torch.no_grad():
+                        out = model(batch)
+
+                    multiplier = (2*batch['right']-1)
+                    pred_cam = out['pred_cam']
+                    pred_cam[:,1] = multiplier*pred_cam[:,1]
+                    box_center = batch["box_center"].float()
+                    box_size = batch["box_size"].float()
+                    img_size = batch["img_size"].float()
+                    multiplier = (2*batch['right']-1)
+                    scaled_focal_length = model_cfg.EXTRA.FOCAL_LENGTH / model_cfg.MODEL.IMAGE_SIZE * img_size.max()
+                    pred_cam_t_full = cam_crop_to_full(pred_cam, box_center, box_size, img_size, scaled_focal_length).detach().cpu().numpy()
+
+                    # Render the result
+                    batch_size = batch['img'].shape[0]
+                    for n in range(batch_size):
+                        # Get filename from path img_path
+                        #img_fn, _ = os.path.splitext(os.path.basename(img_path))
+                        person_id = int(batch['personid'][n])
+                        white_img = (torch.ones_like(batch['img'][n]).cpu() - DEFAULT_MEAN[:,None,None]/255) / (DEFAULT_STD[:,None,None]/255)
+                        input_patch = batch['img'][n].cpu() * (DEFAULT_STD[:,None,None]/255) + (DEFAULT_MEAN[:,None,None]/255)
+                        input_patch = input_patch.permute(1,2,0).numpy()
+
+                        regression_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
                                                 out['pred_cam_t'][n].detach().cpu().numpy(),
-                                                white_img,
+                                                batch['img'][n],
                                                 mesh_base_color=LIGHT_BLUE,
                                                 scene_bg_color=(1, 1, 1),
-                                                side_view=True)
-                        final_img = np.concatenate([input_patch, regression_img, side_img], axis=1)
-                    else:
-                        final_img = np.concatenate([input_patch, regression_img], axis=1)
+                                                )
 
-                    #cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_{person_id}.png'), 255*final_img[:, :, ::-1])
+                        if args.side_view:
+                            side_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                                                    out['pred_cam_t'][n].detach().cpu().numpy(),
+                                                    white_img,
+                                                    mesh_base_color=LIGHT_BLUE,
+                                                    scene_bg_color=(1, 1, 1),
+                                                    side_view=True)
+                            final_img = np.concatenate([input_patch, regression_img, side_img], axis=1)
+                        else:
+                            final_img = np.concatenate([input_patch, regression_img], axis=1)
 
-                    # Add all verts and cams to list
-                    verts = out['pred_vertices'][n].detach().cpu().numpy()
-                    is_right = batch['right'][n].cpu().numpy()
-                    verts[:,0] = (2*is_right-1)*verts[:,0]
-                    cam_t = pred_cam_t_full[n]
-                    all_verts.append(verts)
-                    all_cam_t.append(cam_t)
-                    all_right.append(is_right)
+                        #cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_{person_id}.png'), 255*final_img[:, :, ::-1])
 
-                    # Save all meshes to disk
-                    if args.save_mesh:
-                        camera_translation = cam_t.copy()
-                        tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_BLUE, is_right=is_right)
-                        tmesh.export(os.path.join(args.out_folder, f'{frame_num}_{person_id}.obj'))
+                        # Add all verts and cams to list
+                        verts = out['pred_vertices'][n].detach().cpu().numpy()
+                        is_right = batch['right'][n].cpu().numpy()
+                        verts[:,0] = (2*is_right-1)*verts[:,0]
+                        cam_t = pred_cam_t_full[n]
+                        all_verts.append(verts)
+                        all_cam_t.append(cam_t)
+                        all_right.append(is_right)
 
-            # Render front view
-            if args.full_frame and len(all_verts) > 0:
-                misc_args = dict(
-                    mesh_base_color=LIGHT_BLUE,
-                    scene_bg_color=(1, 1, 1),
-                    focal_length=scaled_focal_length,
-                )
-                cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
+                        # Save all meshes to disk
+                        if args.save_mesh:
+                            camera_translation = cam_t.copy()
+                            tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_BLUE, is_right=is_right)
+                            tmesh.export(os.path.join(args.out_folder, f'{frame_num}_{person_id}.obj'))
+                for i in range(frame_skip_count):
+                    # Render front view
+                    if args.full_frame and len(all_verts) > 0:
+                        misc_args = dict(
+                            mesh_base_color=LIGHT_BLUE,
+                            scene_bg_color=(1, 1, 1),
+                            focal_length=scaled_focal_length,
+                        )
+                        cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
 
-                # Overlay image
-                input_img = img_cv2.astype(np.float32)[:,:,::-1]/255.0
-                input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
-                input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
+                        # Overlay image
+                        input_img = frames[i].astype(np.float32)[:,:,::-1]/255.0
+                        input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
+                        input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
 
-                final_frame = cv2.resize(255*input_img_overlay[:, :, ::-1], (512, 512), 
-                   interpolation = cv2.INTER_LINEAR)
-                final_frame = cv2.normalize(final_frame, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8U)
-                video.write(final_frame)
-                #if frame_num == 300:
-                #    break
+                        final_frame = cv2.resize(255*input_img_overlay[:, :, ::-1], (512, 512), 
+                           interpolation = cv2.INTER_LINEAR)
+                        final_frame = cv2.normalize(final_frame, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8U)
+                        video.write(final_frame)
+                        #if frame_num == 300:
+                        #    break
     except KeyboardInterrupt:
         print("saving")
         cv2.destroyAllWindows()
