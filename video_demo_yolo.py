80b9e2a8896a9749572b4e8b6c93d91aa12893e3
ntvthuyen <ntvthuyen@gmail.com>
2024-10-20 18:51:19 +0800

    Add yolo detection support

---
+++ video_demo_yolo.py
@@ -0,0 +1,300 @@
+from pathlib import Path
+import torch
+import argparse
+import os
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+from hamer.configs import CACHE_DIR_HAMER
+from hamer.models import HAMER, download_models, load_hamer, DEFAULT_CHECKPOINT
+from hamer.utils import recursive_to
+from hamer.datasets.vitdet_dataset import ViTDetDataset, DEFAULT_MEAN, DEFAULT_STD
+from hamer.utils.renderer import Renderer, cam_crop_to_full
+from hamer.utils.video_frame_processing import get_less_blurry_image
+            
+LIGHT_BLUE=(0.65098039,  0.74117647,  0.85882353)
+
+from vitpose_model import ViTPoseModel
+
+import json
+from typing import Dict, Optional
+
+def main():
+    parser = argparse.ArgumentParser(description='HaMeR demo code')
+    parser.add_argument('--checkpoint', type=str, default=DEFAULT_CHECKPOINT, help='Path to pretrained model checkpoint')
+    parser.add_argument('--img_folder', type=str, default='images', help='Folder with input images')
+    parser.add_argument('--out_folder', type=str, default='out_demo', help='Output folder to save rendered results')
+    parser.add_argument('--side_view', dest='side_view', action='store_true', default=False, help='If set, render side view also')
+    parser.add_argument('--full_frame', dest='full_frame', action='store_true', default=True, help='If set, render all people together also')
+    parser.add_argument('--save_mesh', dest='save_mesh', action='store_true', default=False, help='If set, save meshes to disk also')
+    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for inference/fitting')
+    parser.add_argument('--rescale_factor', type=float, default=2.0, help='Factor for padding the bbox')
+    parser.add_argument('--body_detector', type=str, default='yolo', choices=['vitdet', 'regnety', 'yolo'], help='Using regnety improves runtime and reduces memory')
+    parser.add_argument('--file_type', nargs='+', default=['*.jpg', '*.png'], help='List of file extensions to consider')
+    parser.add_argument('--video_path', type=str, default="", help='Video path')
+    parser.add_argument('--yolo_model_path', type=str, default="yolo_weight_best.pt", help='Yolo model path')
+
+
+    args = parser.parse_args()
+
+    # Download and load checkpoints
+    download_models(CACHE_DIR_HAMER)
+    model, model_cfg = load_hamer(args.checkpoint)
+
+    # Setup HaMeR model
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = model.to(device)
+    model.eval()
+
+    # Load detector
+    from hamer.utils.utils_detectron2 import DefaultPredictor_Lazy
+    if args.body_detector == 'vitdet':
+        from detectron2.config import LazyConfig
+        import hamer
+        cfg_path = Path(hamer.__file__).parent/'configs'/'cascade_mask_rcnn_vitdet_h_75ep.py'
+        detectron2_cfg = LazyConfig.load(str(cfg_path))
+        detectron2_cfg.train.init_checkpoint = "https://dl.fbaipublicfiles.com/detectron2/ViTDet/COCO/cascade_mask_rcnn_vitdet_h/f328730692/model_final_f05665.pkl"
+        for i in range(3):
+            detectron2_cfg.model.roi_heads.box_predictors[i].test_score_thresh = 0.25
+        detector = DefaultPredictor_Lazy(detectron2_cfg)
+    elif args.body_detector == 'regnety':
+        from detectron2 import model_zoo
+        from detectron2.config import get_cfg
+        detectron2_cfg = model_zoo.get_config('new_baselines/mask_rcnn_regnety_4gf_dds_FPN_400ep_LSJ.py', trained=True)
+        detectron2_cfg.model.roi_heads.box_predictor.test_score_thresh = 0.5
+        detectron2_cfg.model.roi_heads.box_predictor.test_nms_thresh   = 0.4
+        detector       = DefaultPredictor_Lazy(detectron2_cfg)
+    elif args.body_detector == 'yolo':
+        detector = YOLO(args.yolo_model_path)
+        yolo_predict_conf = 0.25
+        yolo_predict_imgsz=640
+        # implement yolo model config here 
+    # keypoint detector
+    cpm = ViTPoseModel(device)
+
+    # Setup the renderer
+    renderer = Renderer(model_cfg, faces=model.mano.faces)
+
+    # Make output directory if it does not exist
+    os.makedirs(args.out_folder, exist_ok=True)
+
+    # Get all demo images ends with .jpg or .png
+    #img_paths = [img for emnd in args.file_type for img in Path(args.img_folder).glob(end)]
+    
+    cap = cv2.VideoCapture(args.video_path)
+    # Iterate over all images in folder
+    #pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+    #for img_path in img_paths:
+    
+    frame_skip = 6
+    frames = np.array([None]*frame_skip)
+    image_size = (640,640)
+    resize = True
+    video=cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'XVID'),30,(512,512))
+    frame_num = 0
+    frame_skip_count = 0
+    try:
+        while cap.isOpened():
+            frame_num += 1
+            
+            #if frame_skip_count <= 4:
+            flag, frame = cap.read()
+            img_cv2 = frame # cv2.imread(str(img_path))
+            
+            if resize:
+                img_cv2 = cv2.resize(img_cv2, image_size,  
+                    interpolation = cv2.INTER_LINEAR)
+                
+            frames[frame_skip_count] = img_cv2
+            frame_skip_count += 1
+                
+            if frame_skip_count == frame_skip:
+                frame_skip_count = 0
+                idx = get_less_blurry_image(frames)
+                           
+                #flag, frame = cap.read()
+            
+                img_cv2 = frames[idx] # cv2.imread(str(img_path))
+               # Detect humans in image\
+                
+                if args.body_detector == 'yolo':
+                    # add more config
+                    det_out = detector(img_cv2, imgsz=yolo_predict_imgsz, conf=yolo_predict_conf)
+                    img = img_cv2.copy()[:, :, ::-1]
+                    for i in pred_bboxes.size:
+                        tmp = pred_bboxes[i]
+                        pred_bboxes[i][0] = max(0, (tmp[0] - tmp[2]/2)*yolo_predict_imgsz)
+                        pred_bboxes[i][1] = max(0, (tmp[1] - tmp[3]/2)*yolo_predict_imgsz)
+                        pred_bboxes[i][2] = min(yolo_predict_imgsz  - 1, (tmp[0] + tmp[2]/2)*yolo_predict_imgsz)
+                        pred_bboxes[i][3] = min(yolo_predict_imgsz - 1, (tmp[1] + tmp[3]/2)*yolo_predict_imgsz)
+
+
+                    pred_bboxes = det_out.bboxes*yolo_predict_imgsz
+                    pred_scores = det_out.scores
+                    
+
+                else:
+                    det_out = detector(img_cv2)
+                
+                    print(det_out)
+                    img = img_cv2.copy()[:, :, ::-1]
+
+                    det_instances = det_out['instances']
+                
+                    #print(det_instances)
+                    valid_idx = (det_instances.pred_classes==0) & (det_instances.scores > 0.5)
+                    pred_bboxes=det_instances.pred_boxes.tensor[valid_idx].cpu().numpy()
+                    pred_scores=det_instances.scores[valid_idx].cpu().numpy()
+
+                    # Sample output 
+                    # pred_bboxes
+                    #[[  0.      222.1682  236.85825 510.87238]
+                    
+                    # [316.46558 280.56403 511.12195 511.48865]]
+                    # pred_scores
+                    # [0.9519848 0.7241001]
+                    # pred_scores[:, None]
+                    # [[0.9519848]
+                    #  [0.7241001]]
+                # Detect human keypoints for each person
+                vitposes_out = cpm.predict_pose(
+                    img,
+                    [np.concatenate([pred_bboxes, pred_scores[:, None]], axis=1)],
+                )
+
+                bboxes = []
+                is_right = []
+
+                # Use hands based on hand keypoint detections
+                for vitposes in vitposes_out:
+                    left_hand_keyp = vitposes['keypoints'][-42:-21]
+                    right_hand_keyp = vitposes['keypoints'][-21:]
+
+                    # Rejecting not confident detections
+                    keyp = left_hand_keyp
+                    valid = keyp[:,2] > 0.5
+                    if sum(valid) > 3:
+                        bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
+                        bboxes.append(bbox)
+                        is_right.append(0)
+                    keyp = right_hand_keyp
+                    valid = keyp[:,2] > 0.5
+                    if sum(valid) > 3:
+                        bbox = [keyp[valid,0].min(), keyp[valid,1].min(), keyp[valid,0].max(), keyp[valid,1].max()]
+                        bboxes.append(bbox)
+                        is_right.append(1)
+
+                if len(bboxes) == 0:
+                    continue
+
+                boxes = np.stack(bboxes)
+                right = np.stack(is_right)
+
+                # Run reconstruction on all detected hands
+                dataset = ViTDetDataset(model_cfg, img_cv2, boxes, right, rescale_factor=args.rescale_factor)
+                dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
+
+                all_verts = []
+                all_cam_t = []
+                all_right = []
+                
+                for batch in dataloader:
+                    batch = recursive_to(batch, device)
+                    with torch.no_grad():
+                        out = model(batch)
+
+                    multiplier = (2*batch['right']-1)
+                    pred_cam = out['pred_cam']
+                    pred_cam[:,1] = multiplier*pred_cam[:,1]
+                    box_center = batch["box_center"].float()
+                    box_size = batch["box_size"].float()
+                    img_size = batch["img_size"].float()
+                    multiplier = (2*batch['right']-1)
+                    scaled_focal_length = model_cfg.EXTRA.FOCAL_LENGTH / model_cfg.MODEL.IMAGE_SIZE * img_size.max()
+                    pred_cam_t_full = cam_crop_to_full(pred_cam, box_center, box_size, img_size, scaled_focal_length).detach().cpu().numpy()
+
+                    # Render the result
+                    batch_size = batch['img'].shape[0]
+                    for n in range(batch_size):
+                        # Get filename from path img_path
+                        #img_fn, _ = os.path.splitext(os.path.basename(img_path))
+                        person_id = int(batch['personid'][n])
+                        white_img = (torch.ones_like(batch['img'][n]).cpu() - DEFAULT_MEAN[:,None,None]/255) / (DEFAULT_STD[:,None,None]/255)
+                        input_patch = batch['img'][n].cpu() * (DEFAULT_STD[:,None,None]/255) + (DEFAULT_MEAN[:,None,None]/255)
+                        input_patch = input_patch.permute(1,2,0).numpy()
+
+                        regression_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                                                out['pred_cam_t'][n].detach().cpu().numpy(),
+                                                batch['img'][n],
+                                                mesh_base_color=LIGHT_BLUE,
+                                                scene_bg_color=(1, 1, 1),
+                                                )
+
+                        if args.side_view:
+                            side_img = renderer(out['pred_vertices'][n].detach().cpu().numpy(),
+                                                    out['pred_cam_t'][n].detach().cpu().numpy(),
+                                                    white_img,
+                                                    mesh_base_color=LIGHT_BLUE,
+                                                    scene_bg_color=(1, 1, 1),
+                                                    side_view=True)
+                            final_img = np.concatenate([input_patch, regression_img, side_img], axis=1)
+                        else:
+                            final_img = np.concatenate([input_patch, regression_img], axis=1)
+
+                        #cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}_{person_id}.png'), 255*final_img[:, :, ::-1])
+
+                        # Add all verts and cams to list
+                        verts = out['pred_vertices'][n].detach().cpu().numpy()
+                        is_right = batch['right'][n].cpu().numpy()
+                        verts[:,0] = (2*is_right-1)*verts[:,0]
+                        cam_t = pred_cam_t_full[n]
+                        all_verts.append(verts)
+                        all_cam_t.append(cam_t)
+                        all_right.append(is_right)
+
+                        # Save all meshes to disk
+                        if args.save_mesh:
+                            camera_translation = cam_t.copy()
+                            tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_BLUE, is_right=is_right)
+                            tmesh.export(os.path.join(args.out_folder, f'{frame_num}_{person_id}.obj'))
+                for i in range(frame_skip):
+                    # Render front view
+                    if args.full_frame and len(all_verts) > 0:
+                        misc_args = dict(
+                            mesh_base_color=LIGHT_BLUE,
+                            scene_bg_color=(1, 1, 1),
+                            focal_length=scaled_focal_length,
+                        )
+                        cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
+
+                        # Overlay image
+                        input_img = frames[i].astype(np.float32)[:,:,::-1]/255.0
+                        input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
+                        input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
+
+                        final_frame = cv2.resize(255*input_img_overlay[:, :, ::-1], (512, 512), 
+                           interpolation = cv2.INTER_LINEAR)
+                        final_frame = cv2.normalize(final_frame, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8U)
+                        video.write(final_frame)
+                        #if frame_num == 300:
+                        #    break
+    except KeyboardInterrupt:
+
+        print("stop at", frame_num)
+        print("saving")
+        cv2.destroyAllWindows()
+        video.release()
+    except Exception as error:
+        print(error)
+        print("error at", frame_num)
+        print("saving")
+        cv2.destroyAllWindows()
+        video.release()
+
+
+    cv2.destroyAllWindows()
+    video.release()
+
+if __name__ == '__main__':
+    main()
